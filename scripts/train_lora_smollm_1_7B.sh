e63a1592f546c8370d84c4290dfcb2130d6952cf
thedatamonk <rohilpal9763@gmail.com>
2025-11-29 19:28:10 +0000

    Create update script for training SmolLM_1_7B

--- scripts/train_lora_smollm_1_7B.sh
+++ scripts/train_lora_smollm_1_7B.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 set -e
 
-python src/training/train_lora_medquad.py \
+python src/train_lora.py \
   --base-model-name-or-path HuggingFaceTB/SmolLM2-1.7B-Instruct \
   --train-jsonl ./datasets/medquad_train_sft.jsonl \
   --val-jsonl ./datasets/medquad_val_sft.jsonl \
-  --output-dir ./finetuned_outputs/smollm2_1_7B_medquad_lora \
+  --output-dir ./finetuned_outputs/smollm2_1_7b_medquad_lora_full_train \
   --max-seq-length 512 \
   --per-device-train-batch-size 4 \
+  --per-device-eval-batch-size 4 \
   --gradient-accumulation-steps 8 \
-  --learning-rate 2e-4 \
+  --learning-rate 1e-4 \
   --num-train-epochs 2 \
-  --lora-rank 8 \
+  --lora-rank 16 \
+  --lora-alpha 32 \
+  --lora-dropout 0.05 \
   --use-bf16 \
-  --sanity-check
+  --evaluation-strategy "steps" \
+  --eval-steps 200 \
+  --save-steps 200 \
+  --logging-steps 50
